37cd4345fc76290e2db0248a2faf2f1e02f7af9c
Asnetume <ajicliqueres24031997@gmail.com>
2023-05-15 15:12:23 +0700

    update 15-05-2023_15-12

--- sela/src/main/java/com/sela/sela/controller/CustomerController.java
+++ sela/src/main/java/com/sela/sela/controller/CustomerController.java
@@ -1,5 +1,112 @@
 package com.sela.sela.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sela.sela.dto.Response;
+import com.sela.sela.model.User;
+
+@RestController
 public class CustomerController {
+    @Autowired CustomerController cs;
     
+    @GetMapping("/index")
+    public ResponseEntity<Response> index(){
+        try {
+            Response resp = Response.builder()
+            .code("200")
+            .data(cs.index())
+            .message("data index")
+            .build();
+            return new ResponseEntity<Response>(resp, HttpStatus.OK);
+        } catch (Exception e) {
+            Response resp = Response.builder()
+            .code("400")
+            .data(cs.index())
+            .message("Error")
+            .build();
+            return new ResponseEntity<Response>(resp, HttpStatus.NOT_FOUND);
+            }
+    }
+    @PostMapping("/save")
+    public ResponseEntity<Response> register(@RequestBody User body){
+        try {
+            Response resp = Response.builder()
+            .code("200")
+            .data(cs.register(body))
+            .message("data save")
+            .build();
+            return new ResponseEntity<Response>(resp, HttpStatus.OK);
+        } catch (Exception e) {
+            Response resp = Response.builder()
+            .code("400")
+            .data(cs.register(body))
+            .message("Error")
+            .build();
+            return new ResponseEntity<Response>(resp, HttpStatus.NOT_FOUND);
+        }
+    }
+    @GetMapping("{id}")
+    public ResponseEntity<Response> show(@PathVariable Long id){
+        try {
+            Response resp = Response.builder()
+            .code("200")
+            .data(cs.show(id))
+            .message("get data")
+            .build();
+            return new ResponseEntity<Response>(resp, HttpStatus.OK);
+        } catch (Exception e) {
+            Response resp = Response.builder()
+            .code("400")
+            .data(cs.show(id))
+            .message("Error")
+            .build();
+            return new ResponseEntity<Response>(resp, HttpStatus.NOT_FOUND);
+        }
+    }
+    @PutMapping("{id}")
+    public ResponseEntity<Response> update(@RequestBody User body, @PathVariable Long id) {
+        try {
+            Response resp = Response.builder()
+            .code("200")
+            .data(cs.update(id, body))
+            .message("update data")
+            .build();
+            return new ResponseEntity<Response>(resp, HttpStatus.OK);
+        } catch (Exception e) {
+            Response resp = Response.builder()
+            .code("400")
+            .data(cs.update(id, body))
+            .message("Error")
+            .build();
+            return new ResponseEntity<Response>(resp, HttpStatus.NOT_FOUND);
+        }    
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Response> delete(@PathVariable Long id) {
+        try {
+            Response resp = Response.builder()
+            .code("200")
+            .data(cs.delete(id))
+            .message("delete data")
+            .build();
+            return new ResponseEntity<Response>(resp, HttpStatus.OK);
+        } catch (Exception e) {
+            Response resp = Response.builder()
+            .code("400")
+            .data(cs.delete(id))
+            .message("Error")
+            .build();
+            return new ResponseEntity<Response>(resp, HttpStatus.NOT_FOUND);
+        }    
+    }
 }
